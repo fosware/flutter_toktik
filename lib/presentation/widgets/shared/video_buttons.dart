1c5dec0b86645aeaab4dea9aeff38b5ac8922a41
Mario <mario.gonzalez@jalisco.gob.mx>
2023-07-31 14:08:58 -0600

    end app

--- lib/presentation/widgets/shared/video_buttons.dart
+++ lib/presentation/widgets/shared/video_buttons.dart
@@ -1,56 +1,56 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:toktik/config/helpers/human_formats.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 
 class VideoButtons extends StatelessWidget {
   final VideoPost video;
-  
-  const VideoButtons({
-    super.key,
-    required this.video
-  });
+
+  const VideoButtons({super.key, required this.video});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Column(
       children: [
         _CustomIconButtom(
-          value: video.likes, 
-          iconData: Icons.favorite, 
-          iconColor: Colors.red
+            value: video.likes,
+            iconData: Icons.favorite,
+            iconColor: Colors.red),
+        const SizedBox(height: 20),
+        _CustomIconButtom(
+          value: video.views,
+          iconData: Icons.remove_red_eye_outlined,
         ),
-         _CustomIconButtom(
-          value: video.views, 
-          iconData: Icons.remove_red_eye_outlined, 
-          
+        const SizedBox(height: 20),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration(seconds: 3),
+          child: const _CustomIconButtom(
+              value: 0, iconData: Icons.play_circle_fill_outlined),
         )
       ],
     );
   }
 }
 
-
-
 class _CustomIconButtom extends StatelessWidget {
-  
   final int value;
   final IconData iconData;
   final Color? color;
 
-  const _CustomIconButtom({    
-    required this.value, 
-    required this.iconData, 
-    iconColor
-  }): color = iconColor ?? Colors.white;
+  const _CustomIconButtom(
+      {required this.value, required this.iconData, iconColor})
+      : color = iconColor ?? Colors.white;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        IconButton(onPressed: () {}, 
-        icon: Icon(iconData, color: color, size: 30),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(iconData, color: color, size: 30),
         ),
-        Text( HumanFormats.humanReadbleNumber(value.toDouble()) ),
+        if (value > 0) Text(HumanFormats.humanReadbleNumber(value.toDouble())),
       ],
     );
   }
--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,35 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
+import 'package:toktik/presentation/widgets/video/fullscreen_player.dart';
 
 class VideSrollableView extends StatelessWidget {
-
   final List<VideoPost> videos;
 
-  const VideSrollableView({
-    super.key, 
-    required this.videos
-  });
+  const VideSrollableView({super.key, required this.videos});
 
   @override
   Widget build(BuildContext context) {
     return PageView.builder(
-      scrollDirection: Axis.vertical,
-      physics: const BouncingScrollPhysics(),
-      itemBuilder: (context, index) {
-        final VideoPost videoPost = videos[index];
-        return Stack(
-          children: [
-            Positioned(
-              bottom: 20,
-              right: 20,
-              child: VideoButtons(video: videoPost)),
-          ],
-        );
-
-
-      }
-    );
+        scrollDirection: Axis.vertical,
+        physics: const BouncingScrollPhysics(),
+        itemCount: videos.length,
+        itemBuilder: (context, index) {
+          final VideoPost videoPost = videos[index];
+          return Stack(
+            children: [
+              SizedBox.expand(
+                child: FullScreenplayer(
+                  caption: videoPost.caption,
+                  videoUrl: videoPost.videoUrl,
+                ),
+              ),
+              Positioned(
+                  bottom: 20, right: 20, child: VideoButtons(video: videoPost)),
+            ],
+          );
+        });
   }
 }
-
